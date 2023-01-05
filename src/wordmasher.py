57fab304b720001de97b5185dac633a2b4acf508
Devin Vander Stelt <devin@vstelt.com>
2023-01-04 22:48:30 -0500

    add word document replacing function

--- src/wordmasher.py
+++ src/wordmasher.py
@@ -1,9 +1,21 @@
 import pandas as pd
-db = pd.read_excel(path)
+from docx import Document
+from python_docx_replace import docx_replace
+import re
 
-def wordmash(replace_dict):
-    pass
+def wordmash(replace_df):
+    for (i, row) in replace_df.iterrows():
+        doc = Document("template.docx")
+        for col in replace_df.columns:
+            search_replace(doc, col, row[col])
+
+        doc.save(f"project_{i}.docx")
+        break
+
+def search_replace(doc, search, replace):
+    regex = re.compile(search)
+    docx_replace(doc, **{search: replace})
 
 if __name__ == "__main__":
-    replace_dict = parse_excel("path goes here")
-    wordmash(replace_dict)
+    db = pd.read_excel("database.xlsx")
+    wordmash(db)
