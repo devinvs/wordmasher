a5c5ece0603f0179dcd1b9d38ecf56fb9e43ce74
Devin Vander Stelt <devin@vstelt.com>
2023-01-04 23:23:08 -0500

    now it actually works

--- src/wordmasher.py
+++ src/wordmasher.py
@@ -1,13 +1,12 @@
 import pandas as pd
 from docx import Document
 from python_docx_replace import docx_replace
-import re
 
 def wordmash(replace_df):
     for (i, row) in replace_df.iterrows():
         doc = Document("template.docx")
         for col in replace_df.columns:
-            docx_replace(doc, **{search: replace})
+            docx_replace(doc, **{col: row[col]})
 
         doc.save(f"project_{i+1}.docx")
 
